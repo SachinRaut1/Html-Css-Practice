6aa2d5ee09bc0a86838d84844d1e95607d4f21c0
SachinRaut1 <143862112+SachinRaut1@users.noreply.github.com>
2024-11-30 16:18:16 +0530

    Add files via upload

---
+++ anonymousfunction.js
@@ -0,0 +1,14 @@
+// function add(a,b) {
+// 	console.log(a+b);
+// }
+// //calling fun
+// add(3,4);
+
+//2) progran to print the text
+
+function greet(name) {
+	console.log("heloo"+name+" ")
+}
+
+let name =prompt("enter the name");
+greet(name);
